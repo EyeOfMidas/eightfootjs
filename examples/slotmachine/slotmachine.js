64172a789b7c05efe5e639c0965266b524d79417
Midas <eyeofmidas@gmail.com>
2015-03-15 14:57:58 -0400

    adding in some graphical polish

--- examples/slotmachine/slotmachine.js
+++ examples/slotmachine/slotmachine.js
@@ -4,12 +4,18 @@ SlotMachine = function() {
 };
 SlotMachine.prototype.init = function() {
 	EF.System.Assets.addImage("slotwheel.png");
+	EF.System.Assets.addImage("slotbackground.png");
+	EF.System.Assets.addImage("slotbutton.png");
 
 	this.wheels = [
-		new SlotRoller(104, 50),
-		new SlotRoller(336, 50),
-		new SlotRoller(568, 50)
+		new SlotRoller(78, 43),
+		new SlotRoller(231, 43),
+		new SlotRoller(382, 43)
 	];
+	this.background = new EF.Sprite(EF.System.Assets.getImage("slotbackground.png"), 800, 600);
+	this.background.setPosition({x:0, y:0});
+	this.button = new EF.Sprite(EF.System.Assets.getImage("slotbutton.png"), 236, 149);
+	this.button.setPosition({x:520, y:400});
 };
 SlotMachine.prototype.update = function(delta) {
 	for(var i = 0; i < this.wheels.length; i++) {
@@ -19,12 +25,15 @@ SlotMachine.prototype.update = function(delta) {
 };
 SlotMachine.prototype.draw = function() {
 	EF.System.graphics.clearRect(0, 0, EF.System.canvas.width, EF.System.canvas.height);
-	EF.System.Draw.setFillColor("#666666");
+	EF.System.Draw.setFillColor("#000000");
 	EF.System.Draw.rect(0,0,EF.System.canvas.width, EF.System.canvas.height);
 	
 	for(var i = 0; i < this.wheels.length; i++) {
 		this.wheels[i].draw();
 	}
+
+	this.background.draw();
+	this.button.draw();
 
 	EF.System.Draw.setFillColor("#FFFFFF");
 	EF.System.Draw.setFont("20px sans");
